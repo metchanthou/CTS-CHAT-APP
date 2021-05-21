369422065294070281cf9d31475bec2966bafcc9
Student <student@PNCA32-18.pnc.lan>
2021-05-21 10:15:41 +0700

    newfile

--- server.js
+++ server.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-const express = require('express')
-=======
 const express = require('express');
 const app = express();
 
@@ -10,5 +7,14 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send("Hello Project"))
->>>>>>> 918a12bf0c9c84a6ed6fbb8a9b6171844b6431c6
+app.get('/', (req, res) => res.send("Hello Project"));
+
+let listname = [
+    {id:3, name: "davy", message: "Hello"},
+    {id:4, name: "daly", message: "Hello!"},
+    {id:5, name: "dayy", message: "Hello everyone"},
+]
+app.get('/message', (req, res) => {
+    res.send(listname)
+});
+
