a4d59f4b8bad9221be81d902c2de6774b23b3cda
Student <student@PNCA32-18.pnc.lan>
2021-05-23 04:29:03 +0700

    add code

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,11 @@
 
 
-
 function closePart1(event) {
     event.preventDefault();
 
     getpart1.style.display = "none";
     getpart2.style.display = "block";
-}
+};
 
 function notYet (event) {
     event.preventDefault();
@@ -15,14 +14,12 @@ function notYet (event) {
     getpart1.style.display = "flex";
     getpart2.style.display = "none";
     
-}
+};
 
-function signbutton (event) {
-    const getpart3 = document.querySelector(".part3");
+function signButton (event) {
     getpart3.style.display = "block";
     getpart2.style.display = "none";
-
-}
+};
 
 function requestMessage (res) {
 
@@ -30,7 +27,6 @@ function requestMessage (res) {
     console.log(data)
     const resiveChat = document.querySelector(".resive");
     
-
     if (resiveChat !==null) {
         resiveChat.remove();
     }
@@ -44,46 +40,72 @@ function requestMessage (res) {
         p.textContent = sms.name+ ": " + sms.message;
         newResive.appendChild(p);
         resultChat.appendChild(newResive);
-        console.log(result);
+        
         
     }
-}
+};
+
+// function nextButton (event) {
+//     event.preventDefault();
+
+//     let querry = GET_LOGIN_REQUEST + '?username=' + getusername.value + '&password=' + getpassword.value;
+//     axios
+//         .get(querry).then((res) => {
+//             let isCheck= res.data;
+//             if (isCheck){
+//                 getpart1.style.display = "none";
+//                 getpart3.style.display = "block";
+//             }
+//             getText.textContent ="Your name cannot found!";
+//             getText.style.color ="red";
+//         }) 
+// };
+
 
 function newMessage(event) {
-    const inputMessage = document.querySelector("#messages").value;
-    let names = "Your name"
-    let number = 0
+    const inputMessage = document.querySelector("#messages");
+    
 
     let addMessage = {
         id: number,
-        name: names,
-        message: inputMessage,
+        name: getusername.value,
+        message: inputMessage.value,
     }
 
     const URL = "http://localhost:5000/messages";
     axios
         .post(URL, addMessage)
         .then(requestMessage);
-
-}
+};
 
 const URL = "http://localhost:5000/messages";
 axios
     .get(URL)
     .then(requestMessage);
 
+const IP = "192.168.43.154";
+const PORT = 5000;
+const GET_LOGIN_REQUEST = "http://" + IP + ":" + PORT + "/login";
 
 const getpart1 = document.querySelector(".part1");
 const getpart2 = document.querySelector(".part2");
+const getpart3 = document.querySelector(".part3");
+const getText = document.querySelector("#text");
 const getusername = document.querySelector("#username");
+const getpassword = document.querySelector("#yourpassword");
+
+
+
 
 const btnCreate = document.querySelector("#create");
 const btnNotYet = document.querySelector("#not");
 const btnSend = document.querySelector("#enter");
 const btnSign = document.querySelector("#sign-up");
+const btnNext = document.querySelector("#next");
 
 
 btnCreate.addEventListener("click", closePart1);
 btnNotYet.addEventListener("click", notYet);
 btnSend.addEventListener("click", newMessage);
-btnSign.addEventListener("click", signbutton);
+btnSign.addEventListener("click", signButton);
+btnNext.addEventListener("click", nextButton);
--- server.js
+++ server.js
@@ -1,29 +1,49 @@
-const express = require('express');
+
+const express = require("express");
 const app = express();
-
-// const getListname = (JSON.parse(fs.readFileSync('./username.json')));
-
 
 app.listen(process.env.PORT || 5000, () => console.log("Server running..."));
 
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/', (req, res) => res.send("Hello Project"));
+app.get("/", (req, res) => res.send("Hello Project"));
 
 let messageData = [
     {id:1, name: "davy", message: "Hello"},
     {id:2, name: "daly", message: "Hello!"},
     {id:3, name: "dayy", message: "Hello everyone"},
+];
+
+let allUsername = [
+                {"username": "Channary", "password": 1010},
+                {"username": "Chanry", "password": 1111},
+                {"username": "Kunthy", "password": 2222},
+                {"username": "Sinet", "password": 3333},
+                {"username": "Somphors", "password": 4444},
 ]
-app.get('/messages', (req, res) => {
+
+app.get("/messages", (req, res) => {
     res.send(messageData)
 });
 
-app.post('/messages', (req, res) =>{
+app.post("/messages", (req, res) =>{
     let  getMessage = req.body;
     messageData.push(getMessage);
     res.send(messageData)
-})
+});
 
+app.get("/login", (req, res) =>{
+    let username = req.query.username;
+    let password = req.query.password;
+    
+    let isCheck = false;
+    for (let name of allUsername) {
+        if (name.username === username && name.password === parseInt(password)) {
+            isCheck = true
+        }
+        res.send(isCheck);
+        console.log(name);
+    }
+})
